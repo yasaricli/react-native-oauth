7af6810ad51404f2e298554e5fd08d0af0337669
Yasar icli <yasaricli@gmail.com>
2019-09-10 10:23:54 +0300

    revoke changes and fixed facebook google

--- android/src/main/java/io/fullstack/oauth/OAuthManagerModule.java
+++ android/src/main/java/io/fullstack/oauth/OAuthManagerModule.java
@@ -114,8 +114,13 @@ class OAuthManagerModule extends ReactContextBaseJavaModule {
       final String authVersion = (String) cfg.get("auth_version");
       Activity activity = this.getCurrentActivity();
       FragmentManager fragmentManager = activity.getFragmentManager();
-      String callbackUrl = "https://localhost/" + providerName;
-      
+      String callbackUrl;
+      if (providerName.equals("facebook")) {
+        callbackUrl = "https://localhost/" + providerName;
+      } else {
+        callbackUrl = "http://localhost/" + providerName;
+      }
+
       OAuthManagerOnAccessTokenListener listener = new OAuthManagerOnAccessTokenListener() {
         public void onRequestTokenError(final Exception ex) {
           Log.e(TAG, "Exception with request token: " + ex.getMessage());
